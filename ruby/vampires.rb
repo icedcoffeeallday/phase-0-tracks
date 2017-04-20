e614e670d530b5d081606e9fc0219f9aab4cfa47
Andrea Scott <andreahaleyscott+git@gmail.com>
2017-04-19 21:19:36 -0700

    Initial commit to add questions and user input.

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "Oh, cool. I\"m bad at math. What year were you born?"
+year_born = gets.chomp
+
+puts "We're going to have a special lunch for your interview, and our cafeteria is famous for its garlic bread. Should we order some for you?"
+garlic_preference = gets.chomp
+
+puts "Almost done! We have great benefits, too. Would you like to enroll in our health insurance?"
+insurance_preference = gets.chomp

a1d9638382d50547438a13d55191872f2141ea29
Andrea Scott <andreahaleyscott+git@gmail.com>
2017-04-19 22:04:01 -0700

    Add case statement.

---
+++ ruby/garlic.rb
@@ -0,0 +1,6 @@
+wolves_like_sunshine = true
+wolves_like_garlic = true
+vampires_like_sunshine = false
+vampires_like_garlic = false
+
+puts !(wolves_like_sunshine && wolves_like_garlic)
--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,13 +2,47 @@ puts "What is your name?"
 name = gets.chomp
 
 puts "How old are you?"
-age = gets.chomp
+stated_age = gets.chomp.to_i
 
 puts "Oh, cool. I\"m bad at math. What year were you born?"
-year_born = gets.chomp
+year_born = gets.chomp.to_i
+calculated_age = 2017 - year_born
 
 puts "We're going to have a special lunch for your interview, and our cafeteria is famous for its garlic bread. Should we order some for you?"
-garlic_preference = gets.chomp
+likes_garlic = gets.chomp
 
 puts "Almost done! We have great benefits, too. Would you like to enroll in our health insurance?"
-insurance_preference = gets.chomp
+wants_insurance = gets.chomp
+
+#p name
+#p stated_age
+#p year_born
+#p calculated_age
+#p likes_garlic
+#p wants_insurance
+
+case
+when
+  (calculated_age == stated_age) && (likes_garlic = 'yes' || wants_insurance = 'yes')
+  vampire_status = "Probably NOT a vampire."
+  #employee got their age right, and is willing to eat garlic bread or sign up for insurance, 
+  #the result is “Probably not a vampire.”
+when 
+  (calculated_age != stated_age) && (likes_garlic = 'no' || wants_insurance = 'no')
+  vampire_status = "Probably a vampire."
+  #employee got their age wrong, and hates garlic bread or waives insurance,
+  # the result is “Probably a vampire.”
+when
+  (calculated_age != stated_age) && likes_garlic = 'no' && wants_insurance = 'no'
+  vampire_status = "Almost certainly a vampire."
+  #employee got their age wrong, hates garlic bread, and doesn’t want insurance,
+  #the result is “Almost certainly a vampire.”
+when 
+  name == "Drake Cula" || name = "Tu Fang"
+  vampire_status = "definitely a vampire."
+  #anyone going by the name of “Drake Cula” or “Tu Fang” is clearly a vampire, because come on. 
+  #In that case, you should print “Definitely a vampire.”
+else
+  vampire_status = "Results inconclusive."
+end
+

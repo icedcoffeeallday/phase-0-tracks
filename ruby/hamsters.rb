d8b5c51d1e45c282091fd6bcc72b308d9b15bfb9
Andrea Scott <andreahaleyscott+git@gmail.com>
2017-04-20 19:51:23 -0700

    Add boolean method, print output.

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -10,15 +10,27 @@ hamster_color = gets.chomp
 puts "Is the hamster a good candidate for adoption? (y/n)"
 hamster_adoptability = gets.chomp
 
+def boo (x)
+  if x == 'y'
+    true
+  else false
+end
+end
+
+hamster_adoptability = boo (hamster_adoptability)
+
 puts "Estimated age?"
 hamster_age = gets.chomp.to_i
 if hamster_age == 0
   hamster_age = nil
 end
 
-#testing!
-p hamster_name
-p hamster_volume_tolerance
-p hamster_color
-p hamster_adoptability
-p hamster_age
+puts "Hamster's name is #{hamster_name}"
+puts "Hamster likes a volume of #{hamster_volume_tolerance}."
+puts "Hamster is #{hamster_color} color."
+puts "Is hamster a good candidate for adoption? #{hamster_adoptability}"
+if hamster_age == nil
+  puts "Hamster age is unknown."
+else
+  puts "Hamster is approximately #{hamster_age}."
+end

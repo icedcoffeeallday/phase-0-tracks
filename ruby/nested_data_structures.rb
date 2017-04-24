f3de9d72bf900990dd38e53a4f191679bd49e4a4
Andrea Scott <andreahaleyscott+git@gmail.com>
2017-04-24 16:06:09 -0700

    Finish data structure, start testing outputs.

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,51 +1,57 @@
 #Sales deal tracker
 my_deals = {
   customer_1: {
-      name: 'Acme Co.',
+      name: 'Alpha Co.',
       priority: 'High',
-      main_contact: 'Jane Smits'
+      main_contact: 'Jane Smits',
         deals: {
           deal1: {
             deal_name: '10K Widgets and Batteries',
-            deal_value: 20,000,
+            deal_value: 20000,
               product_interest: [
                     product_1: {
                       product_name: 'Widgets',
                       product_type: 'Hardware'
-                    }
+                    }, #close product1
                     product_2: {
                       product_name: 'Widget Washers',
                       product_type: 'Parts'
-                    }
+                    }, #close product2
                     product_3: {
-                      product_name: 'Widget Batteries'
+                      product_name: 'Widget Batteries',
                       product_type: 'Batteries'
-                    }
-                #] close product list
-          #} close deal1
-        #} close all deals
-      #}close name,
-  #} close customer1
-  #customer_2: {
-      #name
-      #priority
-      #main contact
-        #deals: {
-          #deal1: {
-            #deal name:
-            #deal value:
-              #product interest [
-                    #product 1: {
-                      #name:
-                      #type:
-                    #}
-                    #product 2: {
-                      #name:
-                      #type:
-                    #}
-                #] close product list
-          #} close deal1
-        #} close all deals
-      #}close name,
-  #} close customer2
-#}close main hash
+                    }, #close product3
+                ] #close product list
+          }, #close deal1
+          deal2: {
+            deal_name: 'Upsell 20 Batteries',
+            deal_value: 200,
+              product_interest: [
+                product_1: {
+                  product_name: 'Widget Batteries',
+                  product_type: 'Batteries'
+                  } #close product1
+              ]#close product list
+          } #close deal2
+        } #close all deals
+      }, #close customer_1
+  customer_2: {
+    name: 'Bravo Co.',
+    priority: 'Low',
+    main_contact: 'Taylor Davis',
+        deals: {
+          deal1: {
+            deal_name: 'Prospecting',
+            deal_value: 1000,
+              product_interest: [
+                product_1: {
+                  product_name: 'Widget Starter Pack',
+                  product_type: 'Hardware'
+                  }
+                ] #close product list
+          } #close deal1
+        } #close all deals
+  } #close customer2
+} #close main hash
+puts "My main deal is worth:"
+puts my_deals[:customer_1][:deal1][:deal_value]

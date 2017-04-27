1faa198be6ff42a9317b7fee835165ee183d5806
Andrea Scott <andreahaleyscott+git@gmail.com>
2017-04-26 21:19:52 -0700

    Add fibbonacci code from Josh driving.

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -12,7 +12,7 @@ easy_out_clause = 0
 
 until easy_out_clause == true
  
- array_name.each do |value| #number == value #array_name.include? (number)
+ array_name.each do |value|
 
   case
       when value == number
@@ -37,3 +37,27 @@ array_index
 end #method end
  
 p search_array(test_array,3)
+
+#Release 1
+
+def fib(x)
+  loopcount = 0
+  fib_array = []
+  next_num = 0
+  while loopcount < x
+    if loopcount == 0
+      fib_array << 0
+    elsif loopcount == 1
+      fib_array << 1
+    else loopcount >= 1
+      next_num = fib_array[-1] + fib_array[-2]
+      fib_array << next_num
+    end
+  loopcount += 1
+end
+return fib_array
+end
+p fib(6)
+p fib(100)
+
+

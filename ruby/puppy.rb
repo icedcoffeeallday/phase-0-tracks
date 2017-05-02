038a8c5f176b603e96354259677f22dccdd7eefd
Andrea Scott <andreahaleyscott+git@gmail.com>
2017-05-01 17:38:22 -0700

    Initial commit for puppy characteristics, behavior.

---
+++ ruby/puppy.rb
@@ -0,0 +1,32 @@
+=begin
+
+
+SPECIES --------------------
+Puppies
+
+CHARACTERISTICS --------------------
+Energy level: 10
+Coordination level: 5
+Fur color: varies
+Eye color: varies
+Name: varies
+Legs: 4
+Ears: 2
+Eyes: 2
+Playful: yes
+Friendly: yes
+
+
+BEHAVIOR --------------------
+Run
+Walk
+Jump
+Eat
+Drink water
+Sleep
+Fetch
+Chase cats
+Sit
+Roll over
+
+=end

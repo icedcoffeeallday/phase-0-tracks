a1e578934d701f3b41a94241f2dc0dc353b4b7bf
Andrea Scott <andreahaleyscott+git@gmail.com>
2017-05-03 19:29:27 -0700

    Release 0 and 1.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,3 +1,5 @@
+#class definition
+
 class Puppy
 
   def fetch(toy)
@@ -5,5 +7,50 @@ class Puppy
     toy
   end
 
+  def speak(num)
+    num = num.to_i
+    num.times do
+      puts "Woof!"
+      end
+  end
+
+  def rollover
+    puts "Rollover!"
+  end
+
+  def dogyears(years)
+    years = years.to_i
+    dog_age = years * 7
+    puts "The puppy is #{dog_age} human years old."
+  end
+
+  def highfive(num)
+    num.times {
+      puts "( ^o^)ノ＼(^_^ )"
+    }
+  end
+
+  def initialize
+    puts "Initializing new puppy instance ..."
+  end
+
+
 end
 
+
+#Driver code
+
+bandit = Puppy.new
+
+bandit.fetch("squeaky toy")
+
+bandit.speak(1)
+
+bandit.rollover
+
+bandit.dogyears(3)
+
+puts "How many high-fives do you want from the puppy?"
+  highfive_num = gets.chomp.to_i
+
+bandit.highfive(highfive_num)

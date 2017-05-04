024ca9f7774b31c3c9252523482a2d4069585b7c
Andrea Scott <andreahaleyscott+git@gmail.com>
2017-05-03 20:00:18 -0700

    Finish release 2.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,4 +1,6 @@
-#class definition
+#Pair programming Andrea Scott and Cassi Gallagher
+
+#puppy class definition
 
 class Puppy
 
@@ -34,23 +36,84 @@ class Puppy
     puts "Initializing new puppy instance ..."
   end
 
+end
+
+# release 2 class
+
+class Ikea
+
+  def initialize
+    puts "Congratulations! You're about to spend your entire weekend assembling furniture!"
+  end
+
+  def shop(furniture)
+    puts "You picked up the last #{furniture}!"
+  end
+
+  def meatballs(num)
+    cost = num * 0.03
+    puts "Score! You just got #{num} meatballs for $#{cost}!"
+  end
+  
+  def instructions
+    puts "Try turning the diagram upside down and reading it that way."
+  end
+
+  def assembly (num)
+    (num-1).times { puts "The allen wrench is around here somewhere!" }
+    puts "Found it!"
+  end
 
 end
 
 
-#Driver code
+
+
+#Puppy Driver code
 
 bandit = Puppy.new
-
 bandit.fetch("squeaky toy")
-
 bandit.speak(1)
-
 bandit.rollover
-
 bandit.dogyears(3)
-
 puts "How many high-fives do you want from the puppy?"
   highfive_num = gets.chomp.to_i
-
 bandit.highfive(highfive_num)
+
+
+
+# Ikea driver code
+
+#method testing
+
+#saturday = Ikea.new
+#saturday.shop("GRUNDTAL/NORRVIKEN")
+#saturday.meatballs(10)
+#saturday.instructions
+#saturday.assembly(4)
+
+#release 2
+
+ikea_array = []
+
+50.times do
+
+ikea_array << Ikea.new
+
+end
+
+ikea_array.each { |i|
+  i.shop("GRUNDTAL/NORRVIKEN")
+  i.meatballs(1)
+  i.instructions
+  i.assembly(2)
+}
+
+
+
+
+
+
+
+
+

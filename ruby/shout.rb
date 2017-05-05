f2d46ff9612555c6da042d7923af0c19cef21ace
Andrea Scott <andreahaleyscott+git@gmail.com>
2017-05-04 22:29:45 -0700

    Complete Release 1 module.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,15 @@
 module Shout
-  # tbd
+
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yelling_happily(words)
+    "Yayyyyyy! " + words + "! :D :D :D"
+  end
+
 end
+
+p Shout.yell_angrily("Up yours, pre-existing conditions")
+
+p Shout.yelling_happily("They all got impeached!??!")

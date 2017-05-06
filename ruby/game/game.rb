e1a98c3d66df1c72646896ecb68c5ee3052028af
Andrea Scott <andreahaleyscott+git@gmail.com>
2017-05-06 15:47:07 -0700

    Build and test guess validation logic.

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -1,6 +1,43 @@
 class Game
 
 #access variables
+    attr_reader :word
+    attr_reader :game_active
+    attr_accessor :guessed_letters #array of letters guessed
+    attr_accessor :counter
+    #guess
+    #guessed word
+    #guess count? only if incremented in different methods
+
+
 
 #methods
 
+def initialize(word)
+  #user-input word to guess
+  @counter = 0
+  #set game to in progress
+end
+
+#def word_validate(word)
+#end
+
+def guess_validate(arr, letter)
+  if arr.length = 0
+    false
+  else arr.include? (letter)   
+  end
+end
+
+def guess_store_increment(arr, letter)
+  arr << letter
+  @counter += 1
+end
+
+def guess(letter)
+#store guessed letter in guessed_letters array
+#increment counter if guessed letter is not in guessed letter array
+#compare letter to word array, get letter's index in word array
+#if letter is in word append to correct index position in word array
+#output: word status --> joined guessed word array with _ sted nil
+end
--- ruby/game/game_spec.rb
+++ ruby/game/game_spec.rb
@@ -1,1 +1,8 @@
 require_relative 'game'
+
+describe Game do
+  let (:Game) {Game.new}
+
+it "checks whether letter has been previously guessed"
+
+end

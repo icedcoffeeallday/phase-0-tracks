d57f55ac420c621291cbf645327d1f17052136eb
Andrea Scott <andreahaleyscott+git@gmail.com>
2017-05-14 21:55:44 -0700

    Work on KeyMatch function.

--- js/algos.js
+++ js/algos.js
@@ -8,17 +8,17 @@
     // - return the longest word or phrase from the list
   //output: the longest phrase from the list of phrases
 
-var phrase_list = ["long phrase","longest phrase","longer phrase"];
-var longest_phrase = "";
+    var phrase_list = ["long phrase","longest phrase","longer phrase"];
+    var longest_phrase = "";
 
-function FindLongestPhrase(phrase_list) {
-    for ( i = 0; i < phrase_list.length; i++) {  
-      if (phrase_list[i].length > longest_phrase.length) {
-        longest_phrase = phrase_list[i];
+    function FindLongestPhrase(phrase_list) {
+        for ( i = 0; i < phrase_list.length; i++) {  
+          if (phrase_list[i].length > longest_phrase.length) {
+            longest_phrase = phrase_list[i];
+            }
+          }
+        console.log(longest_phrase)
         }
-      }
-    console.log(longest_phrase)
-    }
 
 //Release 1: Find matching key/value pairs
 
@@ -26,27 +26,37 @@ function FindLongestPhrase(phrase_list) {
 //inputs: at least two objects with key/value pairs
 //steps:
   // - compare object keys
-  // - if match, compare key values
+  // - if keys match, compare key values
   // - if match between both key and value, return true
   // - else return false
 //output: boolean value that indicates whether objects have matching key/value pairs
 
-var Steven = {name: "Steven", age: 54, location: "San Francisco", occupation: "lawyer"}
-var Tamir = {name: "Tamir", age: 32, location: "San Francisco", occupation: "dentist"}
-var Mittens = {animal: "Cat", legs: 4}
-var Bandit = {animal: "Dog", legs: 4}
+    var Steven = {name: "Steven", age: 54, location: "San Francisco", occupation: "lawyer"}
+    var Tamir = {name: "Tamir", age: 32, location: "San Francisco", occupation: "dentist"}
+    var Mittens = {animal: "Cat", legs: 4}
+    var Bandit = {animal: "Dog", legs: 4}
 
 
-function KeyMatch(obj1,obj2) {
-    for (i = 0; i < obj1.length; i++) {
-      if (key1 === key2) {
-        if (value1 === value2) {
-          true
+    function KeyMatch(obj1,obj2) {
+        var Obj1Keys = Object.keys(obj1);
+        var Obj2Keys = Object.keys(obj2);
+         // console.log(Obj1Keys);
+         // console.log(Obj2Keys);
+        // }
+        for (i = 0; i < obj1.length; i++) {
+          if (obj1[i] == obj2[i]) {
+              true
+            }
+          else false
         }
-      }
-      else false
-    }
-    }
+        }
+    //I got really stuck on this one. I have found a lot of really complicated ways to compare
+    //objects with JS, but they're beyond what I understand. I've wrangled this one for an hour or so,
+    //and will come back to it if I can.
+
+
+
+//Release 2: Generate random test data
 
 
 
@@ -58,4 +68,4 @@ function KeyMatch(obj1,obj2) {
 FindLongestPhrase(phrase_list)
 
 //Release 1: Compare key/value pairs
-KeyMatch(Steven,Tamir)
+console.log(KeyMatch(Steven,Tamir))

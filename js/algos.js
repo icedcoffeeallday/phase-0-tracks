3f4b7606ab561a366aa2f940cbc77cd70b718a26
Andrea Scott <andreahaleyscott+git@gmail.com>
2017-05-14 23:14:00 -0700

    Finish release 2 method.

--- js/algos.js
+++ js/algos.js
@@ -8,16 +8,16 @@
     // - return the longest word or phrase from the list
   //output: the longest phrase from the list of phrases
 
-    var phrase_list = ["long phrase","longest phrase","longer phrase"];
+    var phrase_list = ["meow","kittens","cats"];
     var longest_phrase = "";
 
-    function FindLongestPhrase(phrase_list) {
-        for ( i = 0; i < phrase_list.length; i++) {  
-          if (phrase_list[i].length > longest_phrase.length) {
-            longest_phrase = phrase_list[i];
+    function FindLongestPhrase(array) {
+        for ( i = 0; i < array.length; i++) {  
+          if (array[i].length > longest_phrase.length) {
+            longest_phrase = array[i];
             }
           }
-        console.log(longest_phrase)
+        console.log(longest_phrase) ;
         }
 
 //Release 1: Find matching key/value pairs
@@ -31,26 +31,26 @@
   // - else return false
 //output: boolean value that indicates whether objects have matching key/value pairs
 
-    var Steven = {name: "Steven", age: 54, location: "San Francisco", occupation: "lawyer"}
-    var Tamir = {name: "Tamir", age: 32, location: "San Francisco", occupation: "dentist"}
-    var Mittens = {animal: "Cat", legs: 4}
-    var Bandit = {animal: "Dog", legs: 4}
+    // var Steven = {name: "Steven", age: 54, location: "San Francisco", occupation: "lawyer"}
+    // var Tamir = {name: "Tamir", age: 32, location: "San Francisco", occupation: "dentist"}
+    // var Mittens = {animal: "Cat", legs: 4}
+    // var Bandit = {animal: "Dog", legs: 4}
 
 
-    function KeyMatch(obj1,obj2) {
-        var Obj1Keys = Object.keys(obj1);
-        var Obj2Keys = Object.keys(obj2);
-         // console.log(Obj1Keys);
-         // console.log(Obj2Keys);
-        // }
-        for (i = 0; i < obj1.length; i++) {
-          if (obj1[i] == obj2[i]) {
-              true
-            }
-          else false
-        }
-        }
-    //I got really stuck on this one. I have found a lot of really complicated ways to compare
+    // function KeyMatch(obj1,obj2) {
+    //     var Obj1Keys = Object.keys(obj1);
+    //     var Obj2Keys = Object.keys(obj2);
+    //      // console.log(Obj1Keys);
+    //      // console.log(Obj2Keys);
+    //     // }
+    //     for (i = 0; i < obj1.length; i++) {
+    //       if (obj1[i] == obj2[i]) {
+    //           true
+    //         }
+    //       else false
+    //     }
+    //     }
+    //NOTE: I got really stuck on this one. I have found a lot of really complicated ways to compare
     //objects with JS, but they're beyond what I understand. I've wrangled this one for an hour or so,
     //and will come back to it if I can.
 
@@ -66,38 +66,37 @@
 //  - puts words into array
 //output: a number of random alpha character "words" in an array; number is set by the integer input.
 
-      var randomNumber = Math.floor(Math.random() * 11);
-      var randomLetterFinder = Math.floor(Math.random() * 27);
       var letters = "abcdefghijklmnopqrstuvwxyz" ;
-      var letterArray = letters.split('');
+      var randomletter = ""
       var word = "";
       var wordGroup = [] ;
 
 
     function RandomWordArray(int){
 
-      for (i = 0; i <= int; i++) {
-        for ( i = 0; i < randomNumber; i++ ) {
-          word += letterArray[randomLetterFinder]
-          word += letterArray[randomLetterFinder]
-          console.log(word);
-        }
+      while ( wordGroup.length <= int - 1 ) {
+        while ( word.length < Math.floor(Math.random() * 11)) {
+          randomletter = letters[Math.floor(Math.random() * 26)]
+          word += randomletter ;
+          }
         wordGroup.push(word);
+        wordGroup;
       }
-      console.log(wordGroup);
+      //console.log(wordGroup);
     }
-//Inner loop to randomly generate words is only selecting a single letter, not iterating through array to select multiple different letters.
-//Sometimes loop goes crazy and doesn't stop.
+//
 
 
 
 //------------------Driver Code-----------------------//
 
 //Release 0: Find Longest Phrase
-FindLongestPhrase(phrase_list)
+  //FindLongestPhrase(phrase_list)
 
 //Release 1: Compare key/value pairs
-console.log(KeyMatch(Steven,Tamir))
+  //console.log(KeyMatch(Steven,Tamir))
 
 //Release 2: Random word array
-RandomWordArray(2)
+  RandomWordArray(3) 
+  console.log(wordGroup) 
+  FindLongestPhrase(wordGroup) 

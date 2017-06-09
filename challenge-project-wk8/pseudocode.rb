51e75037e9a246011c95a075d39a909e4515b20f
Andrea Scott <andreahaleyscott+git@gmail.com>
2017-06-08 19:32:04 -0700

    Start pseudocode file, flesh out project background.

---
+++ challenge-project-wk8/pseudocode.rb
@@ -0,0 +1,33 @@
+#Methods
+
+#reader:
+#end_date
+
+#Initialize
+  #Instance variables
+    #item_name
+    #use_duration
+    #item_total_cost
+    #uses
+    #cost_per_use
+    #duration_start_date
+
+#Add item
+
+#Add duration
+
+#Edit item
+
+#Edit duration
+
+#Remove item
+
+#Calculate End Date (use_duration,duration_start_date)
+
+#Log use
+
+#Delete use
+
+#Calculate Cost Per Use
+
+#Output
